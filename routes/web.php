0d7e8eb169f0dd22397828964c50acec34ec5095
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-03-30 17:41:41 -0300

    retry helper

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,21 @@ Route::get('{post:title}', [PostController::class, 'showByTitle']);
 Route::prefix('admin')->as('admin.')->group(function (){
     Route::get('test', fn() => 'ok')->name('teste');
 });
+
+
+
+function teste($numberOfCalls)
+{
+    if($numberOfCalls === 2) {
+        return 'OK';
+    }
+
+    throw new \Exception;
+}
+
+Route::get('api/retry-test', function(){
+    $result = retry(3, function($tries){
+        return teste($tries);
+    }, 5);
+    return $result;
+});
